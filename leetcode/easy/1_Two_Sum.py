095e4db9c3e8b6d5faf98da4063ecc314d26843d
Anik Adhikari <developwithanik@gmail.com>
2025-06-27 02:01:39 +0530

    add leetcode

---
+++ leetcode/easy/1_Two_Sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+
+        for ind, val in enumerate(nums):
+            diff = target - val
+
+            if diff in hashmap:
+                return [ind, hashmap[diff]]
+
+            hashmap[val] = ind
+
+        return []
+
